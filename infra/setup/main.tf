e05127c07af4dde872f3ddbf14a52c3a5cac7c4d
erichwang <hwangchanny97@gmail.com>
2025-03-26 00:33:34 +0900

    added terraform

---
+++ infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "devops-recipe-app-tf-state-396608768277"
+    key            = "tf-state-setup"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBY    = "Terraform/setup"
+    }
+  }
+}
